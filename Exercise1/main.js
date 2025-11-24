ec20b70e00a2842a4ac39ee93d43bae523a92657
zaakir10 <zaakirhasan10@gmail.com>
2025-11-24 15:49:18 +0300

    JS FIRST EXERCISE

---
+++ Exercise1/main.js
@@ -0,0 +1,22 @@
+let x = 7;
+let y = 3;
+// addition
+console.log(x + y);
+
+// subtraction
+console.log(x - y);
+
+// multiplication
+console.log(x * y);
+
+// division
+console.log(x / y); 
+
+// modulus
+console.log(x % y);
+
+// increment
+console.log(++x);
+
+// decrement
+console.log(--y);
