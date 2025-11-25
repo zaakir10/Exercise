20d898e233d7052c63ac4c34e46a17cb7b7b8049
zaakir10 <zaakirhasan10@gmail.com>
2025-11-25 15:35:31 +0300

    Exercise two comparison operators

---
+++ Exercise2/app.js
@@ -0,0 +1,32 @@
+let a = 15;
+let b = "15"
+
+// Equal to
+console.log(a == b);
+
+// not equal to
+console.log(a != b);
+
+// strictly
+console.log(a === b);
+
+// strictly but not same type
+
+console.log(a !== b);
+
+// greater than
+
+console.log(a < b);
+
+// less than
+
+console.log(a > b);
+
+// greater than or equal
+
+console.log(a <= b);
+
+// less than or equal
+
+console.log(a >= b);
+
