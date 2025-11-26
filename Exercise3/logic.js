69c40dccc31fb88acd2f47ece00a9c0d0f3e2be0
zaakir10 <zaakirhasan10@gmail.com>
2025-11-26 15:10:42 +0300

    Exercise three logic operator

---
+++ Exercise3/logic.js
@@ -0,0 +1,19 @@
+// And operators &&
+let isSunny = true;
+let isWeekend = false;
+
+// &&
+console.log(isSunny && isWeekend);
+
+// OR ||
+
+console.log(isSunny || isWeekend);
+
+// INVERTED !
+
+console.log(!isSunny);
+
+//   !!
+console.log(!isWeekend);
+
+
