c2496fca353515066ebfcffc3db2f67e7082b11c
zaakir10 <zaakirhasan10@gmail.com>
2025-11-27 17:41:15 +0300

    exercise four functions

---
+++ Exercise4/func.js
@@ -0,0 +1,24 @@
+function addTotal (a,b){
+
+    console.log("The Total Sum is " , a + b);
+    
+}
+
+addTotal(10,50);
+addTotal(100 ,19)
+addTotal(45,89)
+addTotal(76,88)
+
+
+// function expresion version
+
+const add = function(a, b) {
+  return a + b;
+};
+console.log(add(2, 3));   
+console.log(add(10, 15)); 
+console.log(add(7, 8));   
+
+
+
+
