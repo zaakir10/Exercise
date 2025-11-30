37cebecf8530bff1bb126ea5f6062ab79927a739
zaakir10 <zaakirhasan10@gmail.com>
2025-11-30 15:03:20 +0300

    ArrayMethod

---
+++ Exercise7/arrayMethods.js
@@ -0,0 +1,45 @@
+let colors = ["black", "red", "green"];
+
+console.log(colors.length);
+
+// after push
+
+colors.push("yellow")
+
+console.log(colors);
+
+// after pop
+console.log("after pop result");
+
+colors.pop()
+
+console.log(colors);
+
+// b unshift 
+
+console.log("unshift");
+
+colors.unshift("white")
+
+console.log(colors);
+
+// console.log("the lenght of array before ");
+console.log(colors.length)
+
+colors.shift();
+
+console.log(colors);
+
+
+// the lenght of an array
+
+
+console.log("the length of arrray is", colors.length);
+
+
+
+
+
+
+
+
