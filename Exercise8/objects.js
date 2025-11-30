c793121692973a8b656c831fda14dfd730a5b0b6
zaakir10 <zaakirhasan10@gmail.com>
2025-11-30 15:30:57 +0300

    abjects

---
+++ Exercise8/objects.js
@@ -0,0 +1,21 @@
+// objects
+
+let cars = {
+
+    Model : "Tx" ,
+    Make : "Toyota" ,
+    Year : 2024
+}
+
+// printing objects way one
+
+console.log(cars);
+
+// way 2
+
+console.log(cars.Make);
+
+console.log(cars.Model);
+
+console.log(cars.Year);
+
