7ae07e17fac1fea62ac08f2598eb3f4521ff3f99
zaakir10 <zaakirhasan10@gmail.com>
2025-11-30 15:57:00 +0300

    Method and objects

---
+++ Exercise9/methodObject.js
@@ -0,0 +1,14 @@
+let cars = {
+
+    Model : "Tx" ,
+    Make : "Toyota" ,
+    Year : 2024,
+    start: function(){
+
+        console.log("the car has start" );
+        
+
+    }
+}
+
+cars.start();
