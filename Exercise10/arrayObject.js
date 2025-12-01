aae9a0dbcae363f72de3d2bb2db84925c5c0ce60
zaakir10 <zaakirhasan10@gmail.com>
2025-12-01 15:02:04 +0300

    arrayobject

---
+++ Exercise10/arrayObject.js
@@ -0,0 +1,11 @@
+let librery = [
+    { Tittle: "Sooyal", Author: "Zaakir", Year: 2021},
+    { Tittle: "Wadani", Author: "Zaakir", Year: 2015},
+    { Tittle: "xamar", Author: "Zaakir", Year: 2018},
+    { Tittle: "77", Author: "Zaakir", Year: 2006}
+
+]
+
+console.log(librery[0].Tittle);
+console.log(librery[1].Author);
+
