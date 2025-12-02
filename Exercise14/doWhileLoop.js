e14a213ce022926b966c0dd8db28d195af3e5b79
zaakir10 <zaakirhasan10@gmail.com>
2025-12-02 15:17:56 +0300

    do while loop

---
+++ Exercise14/doWhileLoop.js
@@ -0,0 +1,21 @@
+// // do while loops
+// // example
+
+// let password;
+// do{
+//     password = prompt("Enter your password")
+    
+
+// } while (password !== "zaakir")
+
+//     console.log("Welcone cade");
+    
+
+// exercise
+
+
+let number;
+do {
+    number = prompt("Enter a number greater than 10:");
+} while (number <= 10);
+console.log("Thank you!");
