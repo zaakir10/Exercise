fbad3e440d235c4a5b47f76dc41e72700e5425ce
zaakir10 <zaakirhasan10@gmail.com>
2025-12-02 14:54:53 +0300

    while loop

---
+++ Exercise13/whileLoop.js
@@ -0,0 +1,26 @@
+// // while loops 
+//  example
+// let i = 0;
+
+// while(i < 5){
+
+//     console.log("iteration number" +i);
+
+//     i++;
+    
+
+// }
+
+// exercise 13 while loop
+
+let i = 5;
+
+while (i > 0){
+
+    console.log("iteration number" + i);
+
+    i--;
+    
+}
+
+
