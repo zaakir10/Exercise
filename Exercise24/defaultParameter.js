3a5d006e11364e9cf193f35c7b8932a8d5ccae90
zaakir10 <zaakirhasan10@gmail.com>
2025-12-07 16:21:41 +0300

    defaultParameter

---
+++ Exercise24/defaultParameter.js
@@ -0,0 +1,8 @@
+function calculateArea(width, height = "width") {
+    return width * height;
+}
+
+// console.log(calculateArea(5, 10)); // 50 (rectangle)
+// console.log(calculateArea(4));     // 16 (square)
+
+calculateArea(5 ,10)
