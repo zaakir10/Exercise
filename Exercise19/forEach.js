353768f8b90792e99f0cba337343f87d576430ee
zaakir10 <zaakirhasan10@gmail.com>
2025-12-07 14:36:01 +0300

    forEach

---
+++ Exercise19/forEach.js
@@ -0,0 +1,15 @@
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+num.forEach(numbers =>{
+
+    console.log("the array num is " + numbers);
+    
+})
+
+console.log("--------Annomos----------");
+
+num.forEach(function(numbers){
+    
+    console.log("the araay is " + numbers);
+    
+})
