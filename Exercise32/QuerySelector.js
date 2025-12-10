18277b7bab8484b303295b63aa31e94af0a226e4
zaakir10 <zaakirhasan10@gmail.com>
2025-12-10 15:19:04 +0300

    querySelector

---
+++ Exercise32/QuerySelector.js
@@ -0,0 +1,12 @@
+ const heading = document.querySelector("#title");
+        console.log("Heading:", heading);
+
+        const button = document.querySelector("#btn");
+        console.log("Button:", button);
+
+        const firstParagraph = document.querySelector(".info");
+        console.log("First paragraph:", firstParagraph);
+
+        // Select multiple elements using querySelectorAll
+        const allParagraphs = document.querySelectorAll(".info");
+        console.log("All paragraphs:", allParagraphs);
